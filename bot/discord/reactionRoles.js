e02966faa4e4497c58b8edaf8b1f182cd4acd9ed
Kyro3400 <66625200+Kyro3400@users.noreply.github.com>
2022-09-26 17:19:05 -0400

    Add files via upload

--- bot/discord/reactionRoles.js
+++ bot/discord/reactionRoles.js
@@ -1,1 +1,14 @@
-#soon!
+let reactionRoles = {
+    '1013126681915047987': { //channelID
+        '1013128971086467102': { //messageID
+            '📣': '1013129719362891826', //announcements
+            '🎉': '1013129772060127303', //giveaway
+            '❔': '1013129810345726054', //poll pongs
+            '👨‍🎓': '1013129826569306195', //staff updates
+            '🧾': '1013129867002388520', //other
+            '📰': '1013129894294724688', //change logs
+        },
+    }
+}
+
+module.exports = reactionRoles;
